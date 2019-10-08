112f751048dcde9a324197eb79c5d3b625b3e7f0
ASO1701001 <1701001@st.asojuku.ac.jp>
2019-10-08 14:03:56 +0900

    GroupControllerを追加

---
+++ src/classes/controllers/GroupController.php
@@ -0,0 +1,12 @@
+<?php
+namespace Classes\controllers;
+
+class GroupController {
+    public static function group_join($group_id) {
+
+    }
+
+    public static function group_create($group_id) {
+
+    }
+}
