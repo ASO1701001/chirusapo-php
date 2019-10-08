c869cf0ca6edef6928cd25c678de31ac526c5219
ASO1701001 <1701001@st.asojuku.ac.jp>
2019-10-08 14:09:14 +0900

    ルーティングを追加

--- public/index.php
+++ public/index.php
@@ -1,21 +1,18 @@
 <?php
-use Application\app\AccountManager;
-use Application\app\TokenManager;
-use Application\lib\Error;
-use Application\lib\Validation;
+
 use Classes\Controllers\IndexController;
 use Classes\Controllers\AccountController;
+use Classes\controllers\TokenController;
+use Classes\controllers\GroupController;
 use Slim\App;
 use Slim\Container;
-use Slim\Http\Request;
 use Slim\Http\Response;
 
 require_once __DIR__.'/../vendor/autoload.php';
-require_once __DIR__.'/../src/classes/lib/Validation.php';
-require_once __DIR__.'/../src/classes/lib/Error.php';
-require_once __DIR__.'/../src/classes/app/functions.php';
-require_once __DIR__.'/../src/classes/app/AccountManager.php';
-require_once __DIR__.'/../src/classes/app/TokenManager.php';
+require_once __DIR__.'/../src/classes/controllers/IndexController.php';
+require_once __DIR__.'/../src/classes/controllers/AccountController.php';
+require_once __DIR__.'/../src/classes/controllers/TokenController.php';
+require_once __DIR__.'/../src/classes/controllers/GroupController.php';
 
 /*
  * GET
@@ -68,64 +65,15 @@ $c['notAllowedHandler'] = function ($c) {
 $app->get('/', IndexController::class.':index');
 
 $app->post('/account/signup', AccountController::class.':sign_up');
-//$app->post('/account/signin', AccountController::class.':sign_in');
-$app->post('/account/signin', function (Request $request, Response $response) {
-    $param = array_escape($request->getParsedBody());
-
-    $user_id = isset($param['user_id']) ? $param['user_id'] : null;
-    $password = isset($param['password']) ? $param['password'] : null;
-
-    $error = [];
-
-    if (is_null($user_id) || is_null($password)) {
-        $result = [
-            'status' => 400,
-            'message' => [
-                Error::$REQUIRED_PARAM
-            ],
-            'data' => null
-        ];
-    } else {
-        $valid_user_id = Validation::fire($user_id, Validation::$USER_ID_OR_EMAIL);
-        $valid_password = Validation::fire($password, Validation::$PASSWORD);
-
-        if (!$valid_user_id || !$valid_password) {
-            if (!$valid_user_id) $error[] = Error::$VALIDATION_USER_ID;
-            if (!$valid_password) $error[] = Error::$VALIDATION_PASSWORD;
-
-            $result = [
-                'status' => 400,
-                'message' => $error,
-                'data' => null
-            ];
-        } else {
-            $id = AccountManager::sign_in($user_id, $password);
-
-            if (!$id) {
-                $result = [
-                    'status' => 400,
-                    'message' => [
-                        Error::$UNKNOWN_USER
-                    ],
-                    'data' => null
-                ];
-            } else {
-                $token = TokenManager::add_token($id);
-
-                $result = [
-                    'status' => 200,
-                    'message' => null,
-                    'data' => [
-                        'token' => $token
-                    ]
-                ];
-            }
-        }
-    }
-
-    return $response->withJson($result);
-});
+$app->post('/account/signin', AccountController::class.':sign_in');
 $app->post('/account/password-reset', AccountController::class.':password_reset');
+
+$app->post('/token/verify-token', TokenController::class.':verify_token');
+
+/*
+$app->post('/group/join', GroupController::class.':group_join');
+$app->post('/group/create', GroupController::class.':group_create');
+*/
 
 /*
 $app->post('/account/signout', AccountController::class.':signout');
