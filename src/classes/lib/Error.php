94092062e6ffc337636c4baf0e09569c0b1ff39c
ASO1701001 <1701001@st.asojuku.ac.jp>
2019-10-18 14:12:43 +0900

    エラーメッセージを追加

--- src/classes/lib/Error.php
+++ src/classes/lib/Error.php
@@ -2,7 +2,12 @@
 namespace Application\lib;
 
 class Error {
+    // etc.
     public static $REQUIRED_PARAM = 'REQUIRED_PARAM';
+    public static $ALLOW_EXTENSION = 'ALLOW_EXTENSION';
+    public static $UPLOAD_FAILED = 'UPLOAD_FAILED';
+    public static $MAIL_SEND = 'MAIL_SEND';
+    // Account
     public static $VALIDATION_USER_ID = 'VALIDATION_USER_ID';
     public static $VALIDATION_USER_NAME = 'VALIDATION_USER_NAME';
     public static $VALIDATION_EMAIL = 'VALIDATION_EMAIL';
@@ -12,8 +17,11 @@ class Error {
     public static $ALREADY_USER_ID = 'ALREADY_USER_ID';
     public static $ALREADY_EMAIL = 'ALREADY_EMAIL';
     public static $UNKNOWN_USER = 'UNKNOWN_USER';
-    public static $MAIL_SEND = 'MAIL_SEND';
+    public static $VALIDATION_LINE_ID = 'VALIDATION_LINE_ID';
+    public static $VALIDATION_INTRODUCTION = 'VALIDATION_INTRODUCTION';
+    // Token
     public static $UNKNOWN_TOKEN = 'UNKNOWN_TOKEN';
+    // Group
     public static $UNKNOWN_GROUP = 'UNKNOWN_GROUP';
     public static $ALREADY_CREATE_GROUP = 'ALREADY_CREATE_GROUP';
     public static $ALREADY_BELONG_GROUP = 'ALREADY_BELONG_GROUP';
