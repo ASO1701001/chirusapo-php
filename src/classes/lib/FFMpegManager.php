89ed67e9d2baaddc8f94e76d89f2a5f777d5db7a
ASO1701001 <1701001@st.asojuku.ac.jp>
2019-10-25 16:58:08 +0900

    pathを修正

--- src/classes/lib/FFMpegManager.php
+++ src/classes/lib/FFMpegManager.php
@@ -14,10 +14,10 @@ class FFMpegManager {
         try {
             $file_name = random(20).'.jpg';
             $ffmpeg = FFMpeg::create([
-                'ffmpeg.binaries'  => '/usr/local/bin/ffmpeg',
-                'ffprobe.binaries' => '/usr/local/bin/ffprobe',
-                'timeout'          => 3600,
-                'ffmpeg.threads'   => 12
+                'ffmpeg.binaries' => '/usr/bin/ffmpeg',
+                'ffprobe.binaries' => '/usr/bin/ffprobe',
+                'timeout' => 3600,
+                'ffmpeg.threads' => 12
             ]);
             $video = $ffmpeg->open($upload_movie->file);
             $video
